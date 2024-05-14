7fec3c0c4045a842f8c80b2232a51ea46fd7e723
alanguilhermeM <alanguilherme13@outlook.com>
2024-05-14 07:34:00 -0300

    feature: Initial Setup

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <h1 className='text-red-500'>Teste</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Login;
