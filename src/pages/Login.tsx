b4641c3ec26fb17203ae3b5ff0a4269808a50fb8
alanguilhermeM <alanguilherme13@outlook.com>
2024-05-20 15:33:22 -0300

    feature: Done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,8 +14,13 @@ const Login: React.FC = () => {
     },
     validate,
     onSubmit: async (values) => {
-      await login(values)
-      navigate('/profile')
+      const response = await login(values)
+      if (response.status === 200) {
+        navigate('/profile')
+      } else {
+        window.alert(`Falha na requisição, erro: ${response.data}`)
+        console.log(response)
+      }
     },
   });
   return (
@@ -24,9 +29,6 @@ const Login: React.FC = () => {
         onSubmit={formik.handleSubmit}
         className="flex flex-col justify-center items-center w-[438px] h-[534px] border-0 rounded-[18px] bg-[#FFFFFF] shadow-2xl"
       >
-        {/* <h1 className="text-[#02274F] text-[94.81px] font-bold">
-          b2b<span className="text-[#FDCF00]">it</span>
-        </h1> */}
         <img src={logo} alt="logoBit" className="relative bottom-[2rem]" width={300}/>
 
         <div className="flex flex-col w-[385px] relative">
@@ -40,6 +42,10 @@ const Login: React.FC = () => {
             className="w-full h-[54px] bg-[#F1F1F1] rounded-[9px] pt-[18px] pr-[18px] pb-[20.25px] pl-[18px] mb-6"
           />
 
+          {formik.touched.email && formik.errors.email && (
+            <p className="text-red-500 text-sm relative bottom-3 animate-bounce">{formik.errors.email}</p>
+          )}
+
           <label className="font-medium text-[#262626] mb-3">Password</label>
           <input
             name="password"
@@ -47,8 +53,12 @@ const Login: React.FC = () => {
             value={formik.values.password}
             type="password"
             placeholder="************"
-            className="w-full h-[54px] bg-[#F1F1F1] text-white rounded-[9px] pt-[18px] pr-[18px] pb-[20.25px] pl-[18px] mb-6"
+            className="w-full h-[54px] bg-[#F1F1F1] text-black rounded-[9px] pt-[18px] pr-[18px] pb-[20.25px] pl-[18px] mb-6"
           />
+
+          {formik.touched.password && formik.errors.password && (
+            <p className="text-red-700 text-sm relative bottom-3 animate-bounce">{formik.errors.password}</p>
+          )}
 
           <button 
             type="submit" 
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+// import App from '../App';
+import Login from '../pages/Login';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Login />);
+  const linkElement = screen.getByText(/e-mail/i);
   expect(linkElement).toBeInTheDocument();
 });
